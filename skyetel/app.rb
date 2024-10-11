31242b9f6d3fa6737864f4e97d3592646a636b45
David Wilkie <dwilkie@gmail.com>
2024-10-11 16:11:14 +0700

    fix: cleanup entrypoint

--- skyetel/app.rb
+++ skyetel/app.rb
@@ -1,21 +1,9 @@
-require "logger"
 require_relative "config/application"
 
 module App
   class Handler
-    attr_reader :event, :context
-
-    def self.process(event:, context:)
-      logger = Logger.new($stdout)
-      logger.info("## Processing Event")
-      logger.info(event)
-
-      new(event:, context:).process
-    end
-
-    def initialize(event:, context:)
-      @event = event
-      @context = context
+    def self.process(**)
+      new.process
     end
 
     def process
