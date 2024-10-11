3418d2660809f3ec341fd33a0d5cf034b4ff5424
David Wilkie <dwilkie@gmail.com>
2024-10-11 18:44:13 +0700

    fix: Update default config

--- skyetel/config/app_settings.rb
+++ skyetel/config/app_settings.rb
@@ -5,7 +5,7 @@ app_settings = Class.new(EncryptedCredentials::AppSettings) do
   def supported_cities
     @supported_cities ||= begin
       options = {}
-      options[:data] = JSON.parse(ENV.fetch("SUPPORTED_CITIES")) unless ENV["SUPPORTED_CITIES"].empty?
+      options[:data] = JSON.parse(ENV.fetch("SUPPORTED_CITIES")) unless ENV["SUPPORTED_CITIES"].to_s.empty?
       options[:data_file] = Pathname(File.expand_path(fetch(:supported_cities_data_file), __dir__)) if fetch(:supported_cities_data_file)
 
       SupportedCitiesParser.new(**options).parse
--- skyetel/spec/support/env_helpers.rb
+++ skyetel/spec/support/env_helpers.rb
@@ -7,6 +7,16 @@ module EnvHelpers
       allow(ENV).to receive(:fetch).with(key, any_args).and_return(value)
     end
   end
+
+  def stub_app_settings(settings)
+    allow(AppSettings).to receive(:[]).and_call_original
+    allow(AppSettings).to receive(:fetch).with(any_args).and_call_original
+
+    settings.each do |key, value|
+      allow(AppSettings).to receive(:[]).with(key).and_return(value)
+      allow(AppSettings).to receive(:fetch).with(key, any_args).and_return(value)
+    end
+  end
 end
 
 RSpec.configure do |config|
